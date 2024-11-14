1e29532d12a4e3e97bc6af8a084f2a5b4d3e2696
Spencer Lyon <spencerlyon2@gmail.com>
2024-11-13 21:20:47 -0500

    add a second line in hello.py

--- hello.py
+++ hello.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 def main():
     x = np.linspace(0,     10,    100)
     y = np.sin(  x  )
+    y2 = np.cos(x)
     fig, ax = plt.subplots(figsize=(50, 32))
     ax.plot(x,y)
+    ax.plot(x, y2)
     ax.scatter(x,y,s=64,color="k")
 
     return fig
